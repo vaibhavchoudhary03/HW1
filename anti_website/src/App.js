df04f3176ab8e048ae158df33b01a6cf9673f87b
Vaibhav Choudhary <vaibhavchoudhary@Vaibhavs-MacBook-Pro.local>
2023-09-27 16:11:26 -0400

    added h1 tag to homepage

--- anti_website/src/App.js
+++ anti_website/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <h1>Hello everyone! This is my first app</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
